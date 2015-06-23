685b1ccd4c237887eabd192ed4594ceb23b197cf
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2015-06-23 12:14:15 -0500

    styled and changed the login page information

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -90,29 +90,29 @@ setInterval(function() {
     $scope.emailInput = " ";
   }
 
-  var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  // var scene = new THREE.Scene();
+  //     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-      var renderer = new THREE.WebGLRenderer();
-      renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
-      document.body.appendChild( renderer.domElement );
+  //     var renderer = new THREE.WebGLRenderer();
+  //     renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+  //     document.body.appendChild( renderer.domElement );
 
-      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-      var cube = new THREE.Mesh( geometry, material );
-      scene.add( cube );
+  //     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  //     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  //     var cube = new THREE.Mesh( geometry, material );
+  //     scene.add( cube );
 
-      camera.position.z = 5;
+  //     camera.position.z = 5;
 
-      var render = function () {
-        requestAnimationFrame( render );
+  //     var render = function () {
+  //       requestAnimationFrame( render );
 
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
+  //       cube.rotation.x += 0.1;
+  //       cube.rotation.y += 0.1;
 
-        renderer.render(scene, camera);
-      };
+  //       renderer.render(scene, camera);
+  //     };
 
-      render();
+  //     render();
     
   });
